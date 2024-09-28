254b00e146e7638ae3f3de0cbf7dcea4050bfbe8
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2024-09-28 17:21:12 +0100

    Freeze LazyRunner dataclass

--- checkov/lazy_runner_registry.py
+++ checkov/lazy_runner_registry.py
@@ -10,21 +10,26 @@ if TYPE_CHECKING:
     from checkov.common.runners.base_runner import BaseRunner
 
 
-@dataclass
+@dataclass(frozen=True)
 class LazyRunner:
     check_type: CheckType
     module: str | None = None
     cls: str = "Runner"
 
     def __post_init__(self):
-        inferred_module_qualname = f"checkov.{self.check_type}.runner"
-        if self.module == inferred_module_qualname:
+        if self.module == self._inferred_module_qualname:
             raise ValueError(f"Redundant module name provided for {self!r}.")
-        if self.module is None:
-            self.module = inferred_module_qualname
+
+    @property
+    def _inferred_module_qualname(self) -> str:
+        return f"checkov.{self.check_type}.runner"
+
+    @property
+    def _module_qualname(self) -> str:
+        return self.module or self._inferred_module_qualname
 
     def load_runner_class(self) -> type[BaseRunner]:
-        module = import_module(self.module)
+        module = import_module(self._module_qualname)
         return getattr(module, self.cls)
 
     def load_runner(self) -> BaseRunner:
@@ -33,33 +38,33 @@ class LazyRunner:
 
 # sca package runner added during the run method
 LAZY_DEFAULT_RUNNERS: "list[LazyRunner]" = [
-    LazyRunner(check_type=CheckType.ANSIBLE),
-    LazyRunner(check_type=CheckType.ARGO_WORKFLOWS),
-    LazyRunner(check_type=CheckType.ARM),
-    LazyRunner(check_type=CheckType.AZURE_PIPELINES),
-    LazyRunner(check_type=CheckType.BICEP),
-    LazyRunner(check_type=CheckType.BITBUCKET_CONFIGURATION, module="checkov.bitbucket.runner"),
-    LazyRunner(check_type=CheckType.BITBUCKET_PIPELINES),
-    LazyRunner(check_type=CheckType.CDK, cls="CdkRunner"),
-    LazyRunner(check_type=CheckType.CIRCLECI_PIPELINES),
-    LazyRunner(check_type=CheckType.CLOUDFORMATION),
-    LazyRunner(check_type=CheckType.DOCKERFILE),
-    LazyRunner(check_type=CheckType.GITHUB_CONFIGURATION, module="checkov.github.runner"),
-    LazyRunner(check_type=CheckType.GITHUB_ACTIONS),
-    LazyRunner(check_type=CheckType.GITLAB_CONFIGURATION, module="checkov.gitlab.runner"),
-    LazyRunner(check_type=CheckType.GITLAB_CI),
-    LazyRunner(check_type=CheckType.HELM),
-    LazyRunner(check_type=CheckType.JSON, module="checkov.json_doc.runner"),
-    LazyRunner(check_type=CheckType.KUBERNETES),
-    LazyRunner(check_type=CheckType.KUSTOMIZE),
-    LazyRunner(check_type=CheckType.OPENAPI),
-    LazyRunner(check_type=CheckType.SAST),
-    LazyRunner(check_type=CheckType.SCA_PACKAGE, module="checkov.sca_package_2.runner"),
-    LazyRunner(check_type=CheckType.SCA_IMAGE),
-    LazyRunner(check_type=CheckType.SECRETS),
-    LazyRunner(check_type=CheckType.SERVERLESS),
-    LazyRunner(check_type=CheckType.TERRAFORM_PLAN, module="checkov.terraform.plan_runner"),
-    LazyRunner(check_type=CheckType.TERRAFORM),
-    LazyRunner(check_type=CheckType.TERRAFORM_JSON, cls="TerraformJsonRunner"),
-    LazyRunner(check_type=CheckType.YAML, module="checkov.yaml_doc.runner"),
+    LazyRunner(CheckType.ANSIBLE),
+    LazyRunner(CheckType.ARGO_WORKFLOWS),
+    LazyRunner(CheckType.ARM),
+    LazyRunner(CheckType.AZURE_PIPELINES),
+    LazyRunner(CheckType.BICEP),
+    LazyRunner(CheckType.BITBUCKET_CONFIGURATION, module="checkov.bitbucket.runner"),
+    LazyRunner(CheckType.BITBUCKET_PIPELINES),
+    LazyRunner(CheckType.CDK, cls="CdkRunner"),
+    LazyRunner(CheckType.CIRCLECI_PIPELINES),
+    LazyRunner(CheckType.CLOUDFORMATION),
+    LazyRunner(CheckType.DOCKERFILE),
+    LazyRunner(CheckType.GITHUB_CONFIGURATION, module="checkov.github.runner"),
+    LazyRunner(CheckType.GITHUB_ACTIONS),
+    LazyRunner(CheckType.GITLAB_CONFIGURATION, module="checkov.gitlab.runner"),
+    LazyRunner(CheckType.GITLAB_CI),
+    LazyRunner(CheckType.HELM),
+    LazyRunner(CheckType.JSON, module="checkov.json_doc.runner"),
+    LazyRunner(CheckType.KUBERNETES),
+    LazyRunner(CheckType.KUSTOMIZE),
+    LazyRunner(CheckType.OPENAPI),
+    LazyRunner(CheckType.SAST),
+    LazyRunner(CheckType.SCA_PACKAGE, module="checkov.sca_package_2.runner"),
+    LazyRunner(CheckType.SCA_IMAGE),
+    LazyRunner(CheckType.SECRETS),
+    LazyRunner(CheckType.SERVERLESS),
+    LazyRunner(CheckType.TERRAFORM_PLAN, module="checkov.terraform.plan_runner"),
+    LazyRunner(CheckType.TERRAFORM),
+    LazyRunner(CheckType.TERRAFORM_JSON, cls="TerraformJsonRunner"),
+    LazyRunner(CheckType.YAML, module="checkov.yaml_doc.runner"),
 ]
